917466c688dbf7fba528e59a938685e3e8947760
tejaswinibarya19 <tejaswinibarya19@gmail.com>
2025-11-24 14:25:38 +0530

    Route for file upload handling

---
+++ backend/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const uploadController = require('./controllers/uploadController');
+
+router.post('/upload', uploadController.handleUpload);
+
+module.exports = router;

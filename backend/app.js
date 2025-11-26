9a7c89c55026530633f120a052ef6efda8e34164
tejaswinibarya19 <tejaswinibarya19@gmail.com>
2025-11-26 19:47:57 +0530

    Add urlencoded

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const routes = require("./routes");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", routes); 
 
 // error handler
 app.use((err, req, res, next) => {
